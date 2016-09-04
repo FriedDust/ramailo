5afadcd3e3e9f2902bc9116623e05fee6f457aba
FriedDust <fried.dust@gmail.com>
2016-09-04 21:25:28 +0545

    PUT and DELETE methods implementation

--- src/main/java/com/ramailo/GenericRs.java
+++ src/main/java/com/ramailo/GenericRs.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -55,6 +57,32 @@ public class GenericRs {
 		Object result = genericService.create(clazz, object);
 
 		return Response.ok().entity(result).build();
+	}
+	
+	@PUT
+	@Path("/{resource:.*}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response putAction(JsonObject object) {
+		ResourceMeta resource = pathParser.parse(uriInfo);
+		
+		Class<?> clazz = findResource();
+		Object result = genericService.update(clazz, resource.getId(), object);
+
+		return Response.ok().entity(result).build();
+	}
+	
+	@DELETE
+	@Path("/{resource:.*}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response deleteAction(JsonObject object) {
+		ResourceMeta resource = pathParser.parse(uriInfo);
+		
+		Class<?> clazz = findResource();
+		genericService.remove(clazz, resource.getId());
+
+		return Response.ok().build();
 	}
 
 	private Class<?> findResource() {
--- src/main/java/com/ramailo/GenericService.java
+++ src/main/java/com/ramailo/GenericService.java
@@ -1,6 +1,7 @@
 package com.ramailo;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import javax.ejb.Stateless;
@@ -8,7 +9,10 @@ import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 
+import org.apache.commons.beanutils.BeanUtils;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ramailo.exception.ResourceNotFoundException;
 
 /**
  * 
@@ -33,10 +37,43 @@ public class GenericService {
 		try {
 			Object entity = mapper.readValue(object.toString(), clazz);
 			em.persist(entity);
+			em.flush();
 
 			return entity;
 		} catch (IOException e) {
 			throw new RuntimeException();
 		}
+	}
+
+	public Object update(Class<?> clazz, Object pk, JsonObject object) {
+		ObjectMapper mapper = new ObjectMapper();
+		Integer id = Integer.valueOf(pk.toString());
+		try {
+			Object entity = mapper.readValue(object.toString(), clazz);
+			Object existing = em.find(clazz, id);
+			if (existing == null)
+				throw new ResourceNotFoundException();
+
+			BeanUtils.setProperty(entity, "id", id);
+
+			em.merge(entity);
+			em.flush();
+
+			return entity;
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			throw new RuntimeException();
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+
+	public void remove(Class<?> clazz, Object pk) {
+		Integer id = Integer.valueOf(pk.toString());
+		Object existing = em.find(clazz, id);
+		if (existing == null)
+			throw new ResourceNotFoundException();
+
+		em.remove(existing);
+		em.flush();
 	}
 }
