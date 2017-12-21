2031b607c4ede3c62710e8c19b6030d741e8a011
Fried Dust <fried.dust@gmail.com>
2017-12-21 21:17:13 +0545

    Remove unimplemented service

--- src/main/java/com/ramailo/auth/AuthRs.java
+++ src/main/java/com/ramailo/auth/AuthRs.java
@@ -1,6 +1,5 @@
 package com.ramailo.auth;
 
-import javax.inject.Inject;
 import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -10,9 +9,6 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/auth")
 public class AuthRs {
-
-	@Inject
-	private AuthService authService;
 
 	@GET
 	@Path("/login")
