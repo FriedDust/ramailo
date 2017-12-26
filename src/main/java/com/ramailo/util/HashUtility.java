1713b4b726f7c5484b9555ad97d14523d0b62de3
Fried Dust <fried.dust@gmail.com>
2017-12-26 12:25:57 +0545

    Add Logger

--- src/main/java/com/ramailo/util/HashUtility.java
+++ src/main/java/com/ramailo/util/HashUtility.java
@@ -4,6 +4,9 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.ramailo.annotation.Logged;
+
+@Logged
 public class HashUtility {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HashUtility.class);
