46de8d611da5f87ac27006d78518afbc5e2d5dbc
Fried Dust <fried.dust@gmail.com>
2017-12-26 12:08:51 +0545

    Basic Hash Utility

---
+++ src/main/java/com/ramailo/util/HashUtility.java
@@ -0,0 +1,23 @@
+package com.ramailo.util;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HashUtility {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HashUtility.class);
+
+	public String hashPassword(String password) {
+
+		return BCrypt.hashpw(password, BCrypt.gensalt(12));
+	}
+
+	public Boolean checkPassword(String password, String hashedPassword) {
+		try {
+			return BCrypt.checkpw(password, hashedPassword);
+		} catch (IllegalArgumentException e) {
+			return false;
+		}
+	}
+}
